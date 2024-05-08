1203d405f1f0e83c6e6280a6fa5c4ed900a9dc74
Ítalo Santana <italoeugenio539@gmail.com>
2024-05-08 15:18:03 -0300

    Adding class 2

---
+++ maratona-java/src/academy/italo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package academy.italo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String[] args){
+
+        int age = 127;
+        long numeroGrande = 200;
+        double salarioDouble = 2000;
+        float salarioFloat = 1.25F;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'B';
+        String nome = "Grande texto";
+
+    }
+}
