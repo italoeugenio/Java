e88afe1c71769c78fb5c3e7dc7310a4698a53c48
Ítalo Santana <italoeugenio539@gmail.com>
2024-05-08 15:18:57 -0300

    Adding class 3

---
+++ maratona-java/src/academy/italo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package academy.italo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+        String name = "Ítalo";
+        String address = "Qnn 27";
+        float salary = 1218.50F;
+        String date = "12/05/2024";
+
+        System.out.print("I " + name+ ", living in " + address + ", i confirm that i received my wage $" + salary + " on the data " + date);
+    }
+}
