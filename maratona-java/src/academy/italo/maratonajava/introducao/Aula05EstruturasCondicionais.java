0a5fdfded36573da02109454658734eaac109b15
Ítalo Santana <italoeugenio539@gmail.com>
2024-05-08 15:19:10 -0300

    Adding class 5

---
+++ maratona-java/src/academy/italo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,13 @@
+package academy.italo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        if (idade >= 18){
+            System.out.println("Pode comprar bebida alcólica");
+        }
+
+        System.out.println("Fora do if");
+    }
+}
