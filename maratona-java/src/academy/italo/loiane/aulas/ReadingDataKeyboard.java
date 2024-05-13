b161b89f18140586c55fca513b14c8436f8cb570
Ítalo Santana <italoeugenio539@gmail.com>
2024-05-13 15:12:59 -0300

    Updating class ReadDataKeyboard

--- maratona-java/src/academy/italo/loiane/aulas/ReadingDataKeyboard.java
+++ maratona-java/src/academy/italo/loiane/aulas/ReadingDataKeyboard.java
@@ -1,2 +1,23 @@
-package academy.italo.loiane;public class ReadingDataKeyboard {
+package academy.italo.loiane.aulas;
+
+import java.util.Scanner;
+
+public class ReadingDataKeyboard {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Write your full name:");
+        String name = scan.nextLine();
+
+        System.out.print("Write your age:");
+        int age = scan.nextInt();
+
+        System.out.print("Write your height:");
+        double height = scan.nextDouble();
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
+    }
 }
