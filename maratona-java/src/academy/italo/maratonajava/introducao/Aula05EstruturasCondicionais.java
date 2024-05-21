98e9c6fab42873b52f2b6f6becc517cf33bdeba3
Ítalo Santana <112109465+italoeugenio@users.noreply.github.com>
2024-05-21 15:21:40 -0300

    Update Aula05EstruturasCondicionais.java

--- maratona-java/src/academy/italo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ maratona-java/src/academy/italo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -1,13 +1,21 @@
 package academy.italo.maratonajava.introducao;
 
+import java.util.Scanner;
+
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
-        int idade = 20;
+        Scanner scan = new Scanner(System.in);
 
-        if (idade >= 18){
-            System.out.println("Pode comprar bebida alcólica");
+        System.out.print("Write the num1:");
+        int num1 = scan.nextInt();
+
+        System.out.print("Write the num2:");
+        int num2 = scan.nextInt();
+
+        if(num1 > num2){
+            System.out.println(String.format("The number %d is greater than %d", num1, num2));
+        } else if(num2 > num1){
+            System.out.println(String.format("The number %d is greater than %d", num2, num1));
         }
-
-        System.out.println("Fora do if");
     }
 }
