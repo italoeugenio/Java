a57988adf57706c22d56f5c8041335b5b41bfe09
Ítalo Santana <italoeugenio539@gmail.com>
2024-06-14 09:12:56 -0300

    update codes

--- maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -15,9 +15,7 @@ public class Calculadora {
     }
 
     public double divideDoisNumeros(double num1, double num2){
-        if(num1 ==
-
-                0 || num2 == 0){
+        if(num1 == 0 || num2 == 0){
             return 0;
         } else
             return num1/num2;
