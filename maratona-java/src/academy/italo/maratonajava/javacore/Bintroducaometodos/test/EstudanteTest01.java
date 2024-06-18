8e57b2b1329eae019a001df82dfe6122058b2af8
Ítalo Santana <italoeugenio539@gmail.com>
2024-06-18 17:03:53 -0300

    50 - Orientação Objetos - Métodos pt 07 - Parâmetros tipo referência pt 02

--- maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -3,7 +3,7 @@ package academy.italo.maratonajava.javacore.Bintroducaometodos.test;
 import academy.italo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
 import academy.italo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
 
-public class EstudanteTeste01 {
+public class EstudanteTest01 {
     public static void main(String[] args) {
         //Tipo referencia
         Estudante estudante01 = new Estudante();
@@ -18,6 +18,9 @@ public class EstudanteTeste01 {
         estudante02.idade = 16;
         estudante02.sexo = 'F';
 
+       impressora.imprime(estudante01);
+       impressora.imprime(estudante02);
+       //Tipos primitivos passam uma cópia, quando passar parâmetros via referencia afeta o objeto idenpendente
        impressora.imprime(estudante01);
        impressora.imprime(estudante02);
     }
