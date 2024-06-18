202ffe83fd7f1074e9011a6792d30240b7ac1ffb
Ítalo Santana <italoeugenio539@gmail.com>
2024-06-18 16:04:00 -0300

    49 - Orientação Objetos - Métodos pt 06 - Parâmetros tipo referência

---
+++ maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTeste01.java
@@ -0,0 +1,24 @@
+package academy.italo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.italo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.italo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        //Tipo referencia
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Mydoria";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 16;
+        estudante02.sexo = 'F';
+
+       impressora.imprime(estudante01);
+       impressora.imprime(estudante02);
+    }
+}
