b324a3d103e6bf580d0d28d96d19b6038a2f969f
Ítalo Santana <italoeugenio539@gmail.com>
2024-06-20 14:48:33 -0300

    aula 56

--- maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTestCorrecao.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTestCorrecao.java
@@ -5,20 +5,25 @@ import academy.italo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionari
 public class FuncionarioTestCorrecao {
     public static void main(String[] args) {
         FuncionarioCorrecao funcionarioCorrecao = new FuncionarioCorrecao();
-        FuncionarioCorrecao funcionarioCorrecao2 = new FuncionarioCorrecao();
+//        FuncionarioCorrecao funcionarioCorrecao2 = new FuncionarioCorrecao();
         FuncionarioCorrecao funcionarioCorrecao3 = new FuncionarioCorrecao();
 
-        funcionarioCorrecao.nome = "Naruto";
-        funcionarioCorrecao.idade = 15;
-        funcionarioCorrecao.salarios = new double[]{1200, 987.32, 2000};
+        //Teste concluida não alterara o funcionário 1
+        funcionarioCorrecao.setNome("Naruto");
+        funcionarioCorrecao.setIdade(15);
+        funcionarioCorrecao.setSalarios(new double[]{1200, 987.32, 2000});
         funcionarioCorrecao.imprime();
-        funcionarioCorrecao2.nome = "Sasuke";
-        funcionarioCorrecao2.idade = 17;
-        //funcionarioCorrecao2.salarios = new double[]{15000, 987.32,2000};
-        funcionarioCorrecao2.imprime();
-        funcionarioCorrecao3.nome = "Alem4o";
-        funcionarioCorrecao3.idade = 23;
-        funcionarioCorrecao3.salarios = new double[]{100000, 987.32, 2000};
+//        funcionarioCorrecao2.setNome("Sasuke");
+//        funcionarioCorrecao2.setIdade(17);
+//        funcionarioCorrecao2.salarios = new double[]{15000, 987.32,2000};
+//        funcionarioCorrecao2.imprime();
+
+
+        //Preciso arrumar o método set para ele nao sobreescreve a media
+        funcionarioCorrecao3.setNome("Alem4o");
+        funcionarioCorrecao3.setIdade(23);
+        funcionarioCorrecao3.setSalarios(new double[]{100000, 987.32, 2000});
         funcionarioCorrecao3.imprime();
+        System.out.println("Média: " + funcionarioCorrecao3.getMedia() );
     }
 }
