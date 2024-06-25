80513af66387847a9280bc4f3d0cd6a38f158a7d
Ítalo Santana <italoeugenio539@gmail.com>
2024-06-25 15:57:46 -0300

    Fibonnaci sem o loop

--- maratona-java/src/academy/italo/loiane/exercicios/loops/Ex16.java
+++ maratona-java/src/academy/italo/loiane/exercicios/loops/Ex16.java
@@ -1,4 +1,12 @@
 package academy.italo.loiane.exercicios.loops;
 
 public class Ex16 {
+    public static int somar(int num1){
+        int somar = (num1 - (1)) + (num1 - 2);
+        return somar;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(6);
+    }
 }
