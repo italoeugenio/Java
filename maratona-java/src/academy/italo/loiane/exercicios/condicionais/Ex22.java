ec1e49560e8158bcff21ad1e7d7dace6b7403b05
Ítalo Santana <italoeugenio539@gmail.com>
2024-07-29 15:34:31 -0300

    Adicioando exercicio 22 para verificar se é ou não primo

--- maratona-java/src/academy/italo/loiane/exercicios/condicionais/Ex22.java
+++ maratona-java/src/academy/italo/loiane/exercicios/condicionais/Ex22.java
@@ -1,4 +1,29 @@
 package academy.italo.loiane.exercicios.condicionais;
 
+import java.util.Scanner;
+
 public class Ex22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Digite um número para saber se é par ou ímpar ou digite 'sair' para sair:");
+            String input = scanner.nextLine();
+
+            if ("sair".equalsIgnoreCase(input)) {
+                break;
+            }
+
+            try {
+                int numero = Integer.parseInt(input);
+                if (numero % 2 == 0) {
+                    System.out.println("O número " + numero + " é par.");
+                } else {
+                    System.out.println("O número " + numero + " é ímpar.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número ou 'sair'.");
+            }
+        }
+    }
 }
