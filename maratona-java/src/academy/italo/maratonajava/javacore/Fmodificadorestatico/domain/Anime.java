ab35f9ef982bd376272de0823096a2db9732b9ce
Ítalo Santana <italoeugenio539@gmail.com>
2024-08-06 14:52:37 -0300

    Bloco de inicialização estático

---
+++ maratona-java/src/academy/italo/maratonajava/javacore/Fmodificadorestatico/domain/Anime.java
@@ -0,0 +1,35 @@
+package academy.italo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+    //É um bloco que vai ser inicilizado sempre que criar uma nova instancia e sempre que antes do construtor
+    static {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+}
