c31f20a3af6fb840fd772d2b1f2806655fb5805d
Ítalo Santana <italoeugenio539@gmail.com>
2024-08-13 16:22:40 -0300

    EX 15

--- maratona-java/src/academy/italo/soffner/cap1/Ex15.java
+++ maratona-java/src/academy/italo/soffner/cap1/Ex15.java
@@ -1,7 +1,28 @@
 package academy.italo.soffner.cap1;
 
+import java.util.Scanner;
+
 public class Ex15 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int tentativas = 10;
+        int numeroParaAdinhar = 5;
+
+        while(tentativas > 0){
+            System.out.print("Tente adivinhar o número: ");
+            int numero = scanner.nextInt();
+
+            if (numero == numeroParaAdinhar){
+                System.out.println("Parabéns você acertou");
+                break;
+            } else {
+                tentativas--;
+                System.out.printf("VOCÊ ERROU E TEM %d TENTATIVAS\n\n", tentativas);
+            }
+        }
+        if (tentativas == 0){
+            System.out.println("Sinto muito você perdeu =/");
+        }
     }
 }
