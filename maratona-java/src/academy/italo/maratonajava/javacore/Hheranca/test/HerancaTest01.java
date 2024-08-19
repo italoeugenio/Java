6db4cdee5511f295c404eda63164705e7a67a534
Ítalo Santana <italoeugenio539@gmail.com>
2024-08-19 16:59:36 -0300

    Heranca

---
+++ maratona-java/src/academy/italo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,38 @@
+package academy.italo.maratonajava.javacore.Hheranca.test;
+
+import academy.italo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.italo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.italo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco1 = new Endereco();
+        endereco1.setRua("Rua 3");
+        endereco1.setCep("72198873");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Ítalo");
+        pessoa.setCpf("0123456789");
+        pessoa.setEndereco(endereco1);
+
+        Endereco endereco2 = new Endereco();
+        endereco2.setRua("Rua 4");
+        endereco2.setCep("19198198");
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setNome("Camilla");
+        pessoa2.setCpf("8919191956");
+        pessoa2.setEndereco(endereco2);
+
+        pessoa.imprime();
+        pessoa2.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Caio Eugenio");
+        funcionario.setCpf("1891981918");
+        funcionario.setEndereco(endereco1);
+        funcionario.setSalario(20000);
+
+        funcionario.imprime();
+    }
+}
