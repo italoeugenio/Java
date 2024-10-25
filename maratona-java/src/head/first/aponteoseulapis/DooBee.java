51ba9f6189c0365c648faf06fe6da20e0be31a65
Ítalo Santana <italoeugenio539@gmail.com>
2024-10-25 13:54:10 -0300

    Livro do use  acabeca

---
+++ maratona-java/src/head/first/aponteoseulapis/DooBee.java
@@ -0,0 +1,17 @@
+package head.first.aponteoseulapis;
+
+public class DooBee {
+    public static void main(String[] args) {
+        int x = 1;
+
+        while(x < 10){
+            System.out.println("Doo");
+            System.out.println("Bee");
+            x++;
+        }
+
+        if(x == 10){
+            System.out.println("Do333");
+        }
+    }
+}
