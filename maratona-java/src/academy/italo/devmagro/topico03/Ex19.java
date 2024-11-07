982b1c31fd743b2a86f687677915cc18ca4b00cd
Ítalo Santana <italoeugenio539@gmail.com>
2024-11-07 17:05:19 -0300

    ex19

---
+++ maratona-java/src/academy/italo/devmagro/topico03/Ex19.java
@@ -0,0 +1,12 @@
+package academy.italo.devmagro.topico03;
+
+public class Ex19 {
+    public static void main(String[] args) {
+        int numero = 9;
+
+        System.out.println("Tabudada do 9");
+        for (int i = 0; i <= numero; i++){
+            System.out.printf("%d * %d = %d%n", i, numero, i*numero);
+        }
+    }
+}
