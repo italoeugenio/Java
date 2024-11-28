f18f9f3c9f1b9f633b1ee6ef727de8159224dd61
Ítalo Santana <italoeugenio539@gmail.com>
2024-11-28 13:31:16 -0300

    f

--- maratona-java/src/academy/italo/soffner/cap6/Ex06.java
+++ maratona-java/src/academy/italo/soffner/cap6/Ex06.java
@@ -1,4 +1,9 @@
 package academy.italo.soffner.cap6;
 
 public class Ex06 {
+    public static void main(String[] args) {
+        char qualquer = 122;
+        System.out.println(qualquer);
+
+    }
 }
--- maratona-java/src/academy/italo/soffner/cap6/Ex07.java
+++ maratona-java/src/academy/italo/soffner/cap6/Ex07.java
@@ -1,4 +1,17 @@
 package academy.italo.soffner.cap6;
 
 public class Ex07 {
+    public static void main(String[] args) {
+        imprimirQuantidadeDeAsterisco(5);
+    }
+
+    public static void imprimirQuantidadeDeAsterisco(int quantidade){
+        if(quantidade <= 0){
+            System.out.println("Vazio");
+        } else {
+            for(int i = 0; i < quantidade; i++){
+                System.out.print("*");
+            }
+        }
+    }
 }
