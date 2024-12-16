2ad186033b91f1ba878d8ef7fb01c30cc1c41eb1
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-16 14:52:53 -0300

    regex quantificadores

--- maratona-java/src/academy/italo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -10,11 +10,12 @@ public class PatternMatcherTest03 {
         // \s = Todos os espacos em branco /t /n /f /r
         // \S = Tudo menos os espacos em branco
         // \w = a-Z A-, digitos e _
-        // \w = Tudo que nao for: a-Z A-, digitos e _
+        // \W = Tudo que nao for: a-Z A-, digitos e _
         // []
-        String regex = "[abcABC]";
+//        String regex = "[a-zA-C]";
 //        String text1 = "abaaba";
-        String text2 = "cafeBABE";
+        String regex = "0[xX][0-9a-fA-F]";
+        String text2 = "12 0x 0X 0xFFaBC 0x109 0x1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text2);
 
--- maratona-java/src/academy/italo/maratonajava/javacore/Uregex/test/PatternMatcherTest04.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Uregex/test/PatternMatcherTest04.java
@@ -3,7 +3,7 @@ package academy.italo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest02 {
+public class PatternMatcherTest04 {
     public static void main(String[] args) {
         // \d = Todos os digitos
         // \D = Tudo que não for digito
@@ -11,14 +11,20 @@ public class PatternMatcherTest02 {
         // \S = Tudo menos os espacos em branco
         // \w = a-Z A-, digitos e _
         // \W = Tudo que nao for: a-Z A-, digitos e _
-
-        String regex = "\\W";
-//        String text1 = "abaaba";
-        String text2 = "@#hhj212gv_h21kad2";
+        // []
+        // ? Zero ou uma
+        // * Zerou ou mais
+        // + Uma ou mais
+        // {n,m} de n até m
+        // () agrupamento
+        // | Significa "ou" o(v|c)o = ovo ou oco
+        // $
+        String regex = "0[xX]([0-9a-fA-F]{1,8})+(\\s|$)";
+        String texto = "12 0x 0X 0xFFaBC 0x10G 0x1";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(text2);
+        Matcher matcher = pattern.matcher(texto);
 
-        System.out.println("Text: " + text2);
+        System.out.println("Text: " + texto);
         System.out.println("regex " + regex);
         System.out.println("Positions found:");
         while(matcher.find()){
