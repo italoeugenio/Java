3bf356b9fec31386ec038283b665a51b8fd7724f
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-17 13:55:51 -0300

    Finalmente terminei essas malditas aulas de regex, até o demônio tem medo disso ai

--- maratona-java/src/academy/italo/maratonajava/javacore/Uregex/test/PatternMatcherTest05.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Uregex/test/PatternMatcherTest05.java
@@ -21,7 +21,7 @@ public class PatternMatcherTest05 {
         // $
         // . 1.3 = 123, 133, 1%3, ou seja, qualquer valor entre 1 e 3
         String regex = "([a-zA-Z0-9\\._-])+@(gmail\\.com|hotmail\\.com)";
-        String texto = "kirito@hotmail.com, 123jordan@gmail.com, #@!zoro@mail.br, teste@gmail.com, sakura@mail";
+        String texto = "kirito@hotmail.com, 123jordan@gmail.com, #@!zoro@mail.br,teste@gmail.com.br, sakura@mail";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
@@ -31,5 +31,7 @@ public class PatternMatcherTest05 {
         while(matcher.find()){
             System.out.print(matcher.start() +  " " + matcher.group() + "\n");
         }
+
+        // https://regexr.com/
     }
 }
