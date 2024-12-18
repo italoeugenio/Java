032e681e3abb5a222c1ea335d380da1aecbfdd47
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-18 13:58:04 -0300

    140 - Classes Utilitárias - IO pt 03 - FileReader

--- maratona-java/src/academy/italo/maratonajava/javacore/Vio/test/FileReaderTest01.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Vio/test/FileReaderTest01.java
@@ -5,12 +5,18 @@ import java.io.FileReader;
 
 public class FileReaderTest01 {
     public static void main(String[] args) {
+        //Quando voce faz a leitura e a escrita de um arquivo é necessário liberar os recursos por meio meio do close.
         File file = new File("file.txt");
         try(FileReader fr = new FileReader(file)){
-            char[] in = new char[30];
-            fr.read(in);
-            for(char c:in){
-                System.out.print(c);
+//            char[] in = new char[30];
+            //O READ RETORNA -1
+//            fr.read(in);
+//            for(char c:in){
+//                System.out.print(c);
+//            }
+            int i;
+            while((i = fr.read()) != -1){
+                System.out.print((char)i);
             }
         }catch (Exception e){
             e.printStackTrace();
