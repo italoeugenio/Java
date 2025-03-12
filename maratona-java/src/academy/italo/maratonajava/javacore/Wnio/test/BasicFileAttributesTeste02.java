1619385a05cf3cdb702c3b1769fd8a0a042e12bc
Ítalo Santana <italoeugenio539@gmail.com>
2025-03-11 21:26:41 -0300

    f

--- maratona-java/src/academy/italo/maratonajava/javacore/Wnio/test/BasicFileAttributesTeste02.java
+++ maratona-java/src/academy/italo/maratonajava/javacore/Wnio/test/BasicFileAttributesTeste02.java
@@ -1,7 +1,13 @@
 package academy.italo.maratonajava.javacore.Wnio.test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class BasicFileAttributesTeste02 {
     public static void main(String[] args) {
-
+        Path path = Paths.get("pasat2/novo_path.txt");
+//        Files.readAttributes(path, BasicFileAttributes)
     }
 }
---
+++ maratona-java/src/academy/italo/maratonajava/javacore/Wnio/test/DosFileAttributeTeste01.java
@@ -0,0 +1,7 @@
+package academy.italo.maratonajava.javacore.Wnio.test;
+
+public class DosFileAttributeTeste01 {
+    public static void main(String[] args) {
+
+    }
+}
