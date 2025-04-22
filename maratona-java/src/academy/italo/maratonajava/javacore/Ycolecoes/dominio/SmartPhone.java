5c215155d3b2b63b03c999bde9ccac42524cbf08
Ítalo Santana <italoeugenio539@gmail.com>
2025-04-22 12:52:30 -0300

    equals

---
+++ maratona-java/src/academy/italo/maratonajava/javacore/Ycolecoes/dominio/SmartPhone.java
@@ -0,0 +1,36 @@
+package academy.italo.maratonajava.javacore.Ycolecoes.dominio;
+
+public class SmartPhone {
+    private String serialNumber;
+    private String marcar;
+
+    public SmartPhone(String marcar, String serialNumber) {
+        this.marcar = marcar;
+        this.serialNumber = serialNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        SmartPhone smartPhone = (SmartPhone) obj;
+        return serialNumber != null && serialNumber.equals(smartPhone.serialNumber);
+     }
+
+    public String getMarcar() {
+        return marcar;
+    }
+
+    public void setMarcar(String marcar) {
+        this.marcar = marcar;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
---
+++ maratona-java/src/academy/italo/maratonajava/javacore/Ycolecoes/test/EqualTest01.java
@@ -0,0 +1,19 @@
+package academy.italo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.italo.maratonajava.javacore.Ycolecoes.dominio.SmartPhone;
+
+public class EqualTest01 {
+    public static void main(String[] args) {
+        String name = "Italo";
+        String name2 = new String("Italo");
+
+        System.out.println(name == name2); //Refencia de memória
+        System.out.println(name.equals(name2)); //Conteúdo
+
+        SmartPhone s1 = new SmartPhone("1ABC1","iPhone");
+        SmartPhone s2 = s1;
+
+        System.out.println("---SMARTPHONE---");
+        System.out.println(s1.equals(s2));
+    }
+}
